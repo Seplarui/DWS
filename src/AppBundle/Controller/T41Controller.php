6fc941d1a2f382f0e4b0700238aa6f24a422f05c
Sebas Sebas <seplarui@gmail.com>
2016-02-23 22:58:28 +0100

    T41

--- src/AppBundle/Controller/T41Controller.php
+++ src/AppBundle/Controller/T41Controller.php
@@ -5,15 +5,25 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController extends Controller
+class T41Controller extends Controller
 {
     /**
-     * @Route("/hello/{name}", name="hellow")
+     * @Route("/magic/{number}", name="number")
      */
-    public function helloAction($name)
+    public function magicnumberAction($number)
     {
-        return new Response('<html><body>Hello '.$name.'!</body></html>');
+        return new Response('<html><body>Hello '.$number=rand().'!</body></html>');
         
+    }
+    
+    /**
+     * @Route("/dow/{dia}", name="dia")
+     */
+    
+    public function dayOfWeekAction($dia) {
+    	$dow=array("lunes","martes","miercoles","jueves","viernes","sabado","domingo");
+    	return new Response('<html><body>El día de la semana es: '.$dow[$dia].'.');
     }
 }
