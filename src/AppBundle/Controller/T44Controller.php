29d1e3ca4b8cc33cfa992b44e4bf44ec3316c820
Sebas Sebas <seplarui@gmail.com>
2016-02-28 18:18:45 +0100

    Practica 44 Completa

--- src/AppBundle/Controller/T44Controller.php
+++ src/AppBundle/Controller/T44Controller.php
@@ -8,6 +8,23 @@ use Symfony\Component\HttpFoundation\Response;
 
 Class T44Controller extends Controller {
 	
+	public function staticAction($name) {
+		
+		if ($name=='info') {
+			
+			return $this->render('T44/T44info.html.twig', array('name'=>'info'));
+			
+		} elseif ($name=='contact') {
+			
+			return $this->render('T44/T44contact.html.twig',array('name'=>'contact'));
+		
+		} elseif ($name=='about') {
+			
+			return $this->render('T44/T44about.html.twig',array('name'=>'about'));
+					
+		}
+	}
+	
 	
 	
 }
